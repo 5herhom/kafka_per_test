827f659735095c4ac950cc078be679cc7902d06b
5herhom <543872547@qq.com>
2020-09-02 11:41:05 +0800

    Log is ok.

---
+++ reno-kafka/src/main/java/cn/com/sherhom/reno/kafka/LogTest.java
@@ -0,0 +1,12 @@
+package cn.com.sherhom.reno.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LogTest {
+    public static void main(String[] args) {
+        log.info("aaaaaaaaa");
+        log.error("etest");
+        log.debug("dtest");
+    }
+}
---
+++ reno-kafka/src/test/java/cn/com/sherhom/reno/kafka/LogTest.java
@@ -0,0 +1,12 @@
+package cn.com.sherhom.reno.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class LogTest {
+    @Test
+    public void logTest(){
+        log.info("aaaaaaaaa");
+    }
+}
