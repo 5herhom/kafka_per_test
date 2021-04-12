e1b9c28af68a8caca08227bb13d829c1297035e3
5herhom <543872547@qq.com>
2021-04-12 11:20:11 +0800

    Asset

--- reno/reno-common/src/main/java/cn/com/sherhom/reno/common/utils/Asset.java
+++ reno/reno-common/src/main/java/cn/com/sherhom/reno/common/utils/Asset.java
@@ -4,6 +4,7 @@ import cn.com.sherhom.reno.common.exception.RenoException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
+import java.text.MessageFormat;
 import java.util.Collection;
 
 /**
@@ -16,27 +17,45 @@ public class Asset {
         if(o==null)
             throwException(msg);
     }
+    public static void notNull(Object o,String pattern,Object... args) {
+        notNull(o, MessageFormat.format(pattern,args));
+    }
     public static void notEmpty(Collection<?> collection , String msg) {
         if(CollectionUtils.isEmpty(collection))
             throwException(msg);
+    }
+    public static void notEmpty(Collection<?> o,String pattern,Object... args) {
+        notEmpty(o, MessageFormat.format(pattern,args));
     }
     public static void isNotBlank(String s,String msg) {
         if(!StringUtils.isNotBlank(s))
             throwException(msg);
     }
+    public static void isNotBlank(String o,String pattern,Object... args) {
+        isNotBlank(o, MessageFormat.format(pattern,args));
+    }
     public static void isTrue(boolean flag,String msg){
         if(!flag)
             throwException(msg);
     }
+    public static void isTrue(boolean flag,String pattern,Object... args) {
+        isTrue(flag, MessageFormat.format(pattern,args));
+    }
     public static void isFalse(boolean flag,String msg){
         if(flag)
             throwException(msg);
+    }
+    public static void isFalse(boolean flag,String pattern,Object... args) {
+        isFalse(flag, MessageFormat.format(pattern,args));
     }
     public static void isEqual(Object o1,Object o2,String msg){
         if(o1==null&&o2==null)
             return;
         if((o1==null)||!o1.equals(o2))
             throwException(msg);
+    }
+    public static void isEqual(Object o1,Object o2,String pattern,Object... args) {
+        isEqual(o1,o2, MessageFormat.format(pattern,args));
     }
     public static void throwException(String msg){
         log.error(msg);
